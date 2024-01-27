b3e79135b6416b556ce843ab48f51ce597bf6230
Devhell69 <paelalit@gmail.com>
2024-01-27 13:38:07 +0700

    up api auth

--- src/components/functions/auth.jsx
+++ src/components/functions/auth.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export const register = async (value) =>
-  await axios.post(import.meta.env.VITE_VERCEL_URL + "/register", value);
+  await axios.post(import.meta.env.VITE_VERCEL_URL + "/api/register", value);
 
 export const login = async (value) =>
-  await axios.post(import.meta.env.VITE_VERCEL_URL + "/login", value);
+  await axios.post(import.meta.env.VITE_VERCEL_URL + "/api/login", value);
 
 export const currentUser = async (authtoken) =>
   await axios.post(
