674c7321ddadfbbeda326c2c497efc2c2fb81d1c
Devhell69 <paelalit@gmail.com>
2024-01-27 11:58:12 +0700

    up env vercel.json

--- src/components/functions/auth.jsx
+++ src/components/functions/auth.jsx
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 export const register = async (value) =>
-  await axios.post(import.meta.env.VITE_REACT_APP_API_KEY + "/register", value);
+  await axios.post(import.meta.env.VITE_VERCEL_URL + "/register", value);
 
 export const login = async (value) =>
-  await axios.post(import.meta.env.VITE_REACT_APP_API_KEY + "/login", value);
+  await axios.post(import.meta.env.VITE_VERCEL_URL + "/login", value);
 
 export const currentUser = async (authtoken) =>
   await axios.post(
-    import.meta.env.VITE_REACT_APP_API_KEY + "/current-user",
+    import.meta.env.VITE_VERCEL_URL + "/current-user",
     {},
     {
       headers: {
@@ -19,7 +19,7 @@ export const currentUser = async (authtoken) =>
 
   export const currentAdmin = async (authtoken) =>
   await axios.post(
-    import.meta.env.VITE_REACT_APP_API_KEY + "/current-admin",
+    import.meta.env.VITE_VERCEL_URL + "/current-admin",
     {},
     {
       headers: {
