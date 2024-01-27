611a42eeb3f3aeb7b9c02504ec969f5113c0d94c
Devhell69 <paelalit@gmail.com>
2024-01-27 14:13:48 +0700

    up homepage test

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,8 +2,14 @@ const Home = () => {
   return (
     <div className="container text-center my-5">
       <h1>Home Page</h1>
+      <h4 className="mt-4">Admin Test</h4>
+      <span>User : admin</span> <br />
+      <span>Password : 123456</span>
+      <h4 className="mt-4">User Test</h4>
+      <span>User : user</span> <br />
+      <span>Password : 123456</span>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
